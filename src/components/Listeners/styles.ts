1703ea4ee925d2184d365a67c3d88e8088661ebb
RICARDO FREITAS <rmotafreitas@gmail.com>
2023-08-09 00:19:07 +0100

    fix with in listeners

--- src/components/Listeners/styles.ts
+++ src/components/Listeners/styles.ts
@@ -2,24 +2,23 @@ import { StyleSheet } from "react-native";
 import { THEME } from "../../theme";
 
 export const styles = StyleSheet.create({
-    container: {
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "row",
-        borderRadius: 10,
-        paddingHorizontal: 8,
-        paddingVertical: 3,
-        gap: 4,
-        minWidth: 186,
-        marginBottom: 10,
-    },
-    text: {
-        fontSize: THEME.FONT_SIZE.MD,
-        fontFamily: THEME.FONT_FAMILY.BOLD,
-    },
-    foninho: {
-        width: 20,
-        height: 20,
-        objectFit: "contain",
-    },
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    borderRadius: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 3,
+    gap: 4,
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: THEME.FONT_SIZE.MD,
+    fontFamily: THEME.FONT_FAMILY.BOLD,
+  },
+  foninho: {
+    width: 20,
+    height: 20,
+    objectFit: "contain",
+  },
 });
