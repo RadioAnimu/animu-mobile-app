9d4afe9bfddfd21651c26a4bc667b20f6780ca7d
RICARDO FREITAS <rmotafreitas@gmail.com>
2024-02-16 16:07:36 +0000

    Update styles in Cover and HeaderBar components

--- src/components/Cover/styles.ts
+++ src/components/Cover/styles.ts
@@ -1,15 +1,14 @@
 import { StyleSheet } from "react-native";
 import { THEME } from "../../theme";
-import Constans from "expo-constants";
 
 export const styles = StyleSheet.create({
-    image: {
-        resizeMode: "cover",
-        width: 240,
-        height: 240,
-        borderRadius: 12,
-        borderColor: THEME.COLORS.COVER,
-        marginBottom: 11,
-        borderWidth: 5,
-    }
+  image: {
+    resizeMode: "cover",
+    width: 240,
+    height: 240,
+    borderRadius: 12,
+    borderColor: THEME.COLORS.COVER,
+    marginBottom: 11,
+    borderWidth: 5,
+  },
 });
--- src/components/HeaderBar/styles.ts
+++ src/components/HeaderBar/styles.ts
@@ -1,41 +1,39 @@
 import { Dimensions, StyleSheet } from "react-native";
 import { THEME } from "../../theme";
-import Constans from "expo-constants";
-import { Platform } from "expo-modules-core";
 
 export const styles = StyleSheet.create({
-    view: {
-        flexDirection: "column",
-        minHeight: 72,
-    },
-    container: {
-        alignItems: "center",
-        flexDirection: "row",
-        justifyContent: "space-around",
-        backgroundColor: THEME.COLORS.PRIMARY,
-        width: Dimensions.get("window").width,
-        height: 67,
-    },
-    playBtn: {
-        width: 48,
-        height: 48,
-        objectFit: "contain",
-        marginHorizontal: 47,
-    },
-    menuBtn: {
-        width: 27,
-        height: 27,
-        objectFit: "contain",
-    },
-    noteIcon: {
-        width: 27,
-        height: 27,
-        objectFit: "contain",
-    },
-    progressBarView: {
-        height: 5,
-        margin: 0,
-        padding: 0,
-        backgroundColor: THEME.COLORS.SHAPE,
-    },
+  view: {
+    flexDirection: "column",
+    minHeight: 72,
+  },
+  container: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: THEME.COLORS.PRIMARY,
+    width: Dimensions.get("window").width,
+    height: 67,
+  },
+  playBtn: {
+    width: 48,
+    height: 48,
+    objectFit: "contain",
+    marginHorizontal: 47,
+  },
+  menuBtn: {
+    width: 27,
+    height: 27,
+    objectFit: "contain",
+  },
+  noteIcon: {
+    width: 27,
+    height: 27,
+    objectFit: "contain",
+  },
+  progressBarView: {
+    height: 5,
+    margin: 0,
+    padding: 0,
+    backgroundColor: THEME.COLORS.SHAPE,
+  },
 });
