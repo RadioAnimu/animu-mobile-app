fcf6859da088f3458a5a0b7553e2833dce4ede71
RICARDO FREITAS <rmotafreitas@gmail.com>
2024-02-18 18:16:54 +0000

    Refactor app.routes.tsx

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,17 +1,12 @@
-import {
-  DrawerContentComponentProps,
-  DrawerContentScrollView,
-  DrawerItemList,
-  createDrawerNavigator,
-} from "@react-navigation/drawer";
-import { Dimensions, Image, Text, TouchableOpacity, View } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import { Dimensions } from "react-native";
+import { CustomDrawerContent } from "../components/CustomDrawer";
+import { FazerPedido } from "../screens/FazerPedido";
 import { Home } from "../screens/Home";
-import { THEME } from "../theme";
-import { version } from "../../package.json";
 import { UltimasPedidas } from "../screens/UltimasPedidas";
 import { UltimasTocadas } from "../screens/UltimasTocadas";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { FazerPedido } from "../screens/FazerPedido";
+import { THEME } from "../theme";
 
 interface HomeProps {}
 interface UltimasPedidasProps {}
@@ -26,96 +21,6 @@ export type RootStackParamList = {
 };
 
 const { Navigator, Screen } = createDrawerNavigator<RootStackParamList>();
-
-function CustomDrawerContent(props: DrawerContentComponentProps) {
-  const { navigation } = props;
-  return (
-    <DrawerContentScrollView
-      {...props}
-      contentContainerStyle={{
-        justifyContent: "space-between",
-        alignContent: "center",
-        flexDirection: "column",
-        height: "100%",
-      }}
-    >
-      <View>
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("Home");
-          }}
-        >
-          <Image
-            source={require("../assets/logo.png")}
-            style={{
-              height: 100,
-              alignSelf: "center",
-              resizeMode: "contain",
-            }}
-          />
-        </TouchableOpacity>
-        <Text
-          style={{
-            color: THEME.COLORS.WHITE_TEXT,
-            textAlign: "center",
-            padding: 5,
-            fontFamily: THEME.FONT_FAMILY.BOLD,
-            fontSize: THEME.FONT_SIZE.MENU_ITEM,
-          }}
-        >
-          Em construção
-        </Text>
-        {/*
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "flex-start",
-            paddingHorizontal: 20,
-            gap: 5,
-          }}
-        >
-          <MaterialCommunityIcons
-            name="discord"
-            size={20}
-            color={THEME.COLORS.SHAPE}
-          />
-          <Text
-            style={{
-              color: THEME.COLORS.SHAPE,
-              textAlign: "center",
-              fontFamily: THEME.FONT_FAMILY.BOLD,
-              fontSize: THEME.FONT_SIZE.MENU_ITEM,
-            }}
-          >
-            Login Discord
-          </Text>
-        </View>
-        */}
-        <DrawerItemList {...props} />
-      </View>
-      <View
-        style={{
-          marginBottom: 10,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Text
-          style={{
-            color: THEME.COLORS.WHITE_TEXT,
-            textAlign: "center",
-            padding: 5,
-            fontFamily: THEME.FONT_FAMILY.BOLD,
-            fontSize: THEME.FONT_SIZE.FOOTER,
-          }}
-        >
-          Versão v{version} - Desenvolvido com muito ❤️ por Ness.js
-        </Text>
-      </View>
-    </DrawerContentScrollView>
-  );
-}
 
 export function AppRoutes() {
   return (
